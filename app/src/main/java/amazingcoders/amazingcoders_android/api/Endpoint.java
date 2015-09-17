fb8cd98052f7905c6c8aeaa235be807180eed5d5
junwen29 <junwen2992@gmail.com>
2015-09-17 11:48:18 +0800

    update of working json endpoint

--- app/src/main/java/amazingcoders/amazingcoders_android/api/Endpoint.java
+++ app/src/main/java/amazingcoders/amazingcoders_android/api/Endpoint.java
@@ -6,5 +6,8 @@ package amazingcoders.amazingcoders_android.api;
  */
 public class Endpoint {
     public static final String SERVER_URL = "http://10.0.0.2:3000/";
-    public static final String P1_SERVER_URL = SERVER_URL + "v1/";
+    public static final String P1_SERVER_URL = SERVER_URL + "api/v1/";
+
+
+    public static final String DEALS = P1_SERVER_URL + "deals";
 }
