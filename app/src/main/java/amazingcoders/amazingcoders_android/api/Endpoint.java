a87fa358c1bcea1371b39df757e452b96945d195
junwen29 <junwen2992@gmail.com>
2015-09-18 00:42:38 +0800

    updated end point

--- app/src/main/java/amazingcoders/amazingcoders_android/api/Endpoint.java
+++ app/src/main/java/amazingcoders/amazingcoders_android/api/Endpoint.java
@@ -5,8 +5,10 @@ package amazingcoders.amazingcoders_android.api;
  * Created by junwen29 on 9/15/2015.
  */
 public class Endpoint {
-    public static final String SERVER_URL = "http://10.0.0.2:3000/";
-    public static final String P1_SERVER_URL = SERVER_URL + "api/v1/";
+    //    public static final String SERVER_URL = "http://10.0.0.2:3000/";
+    public static final String SERVER_URL = "http://192.168.0.100:3000/";
+
+    public static final String P1_SERVER_URL = SERVER_URL + "api/p1/";
 
 
     public static final String DEALS = P1_SERVER_URL + "deals";
