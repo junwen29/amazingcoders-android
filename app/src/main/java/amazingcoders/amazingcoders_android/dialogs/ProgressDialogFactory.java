f6f5900a48bce3cc8a636fdd561e5f9a1f0a5aea
junwen29 <junwen2992@gmail.com>
2015-09-19 23:49:49 +0800

    created dialog factories

---
+++ app/src/main/java/amazingcoders/amazingcoders_android/dialogs/ProgressDialogFactory.java
@@ -0,0 +1,26 @@
+package amazingcoders.amazingcoders_android.dialogs;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+
+/**
+ * Created by junwen29 on 9/19/2015.
+ */
+public class ProgressDialogFactory {
+
+    public static ProgressDialog create(Context context, int message) {
+        return create(context, message, false);
+    }
+
+    public static ProgressDialog create(Context context, int message, boolean cancelable) {
+        return create(context, message, cancelable, true);
+    }
+
+    public static ProgressDialog create(Context context, int message, boolean cancelable, boolean indeterminate) {
+        ProgressDialog dialog = new ProgressDialog(context);
+        dialog.setMessage(context.getString(message));
+        dialog.setCancelable(cancelable);
+        dialog.setIndeterminate(indeterminate);
+        return dialog;
+    }
+}
