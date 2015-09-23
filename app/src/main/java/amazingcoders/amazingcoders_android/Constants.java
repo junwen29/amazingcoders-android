cdb7ce864c8d06b49b391f4302a750e1ba144fa8
junwen29 <junwen2992@gmail.com>
2015-09-23 12:28:14 +0800

    added burpple constant file

---
+++ app/src/main/java/amazingcoders/amazingcoders_android/Constants.java
@@ -0,0 +1,21 @@
+package amazingcoders.amazingcoders_android;
+
+/**
+ * Created by junwen29 on 9/23/2015.
+ *
+ * created to store constant references
+ */
+public class Constants {
+    public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'Z";
+    public static final String MEAL_DATE_FORMAT = "EEE, MMM d, yyyy";
+    public static final String BIRTHDAY_DATE_FORMAT = "yyyy-MM-dd";
+    public static final String BIRTHDAY_PRETTY_DATE_FORMAT = "dd MMMM yyyy";
+    public static final int LOAD_LIMIT = 10;
+    public static final int BIO_LIMIT = 100;
+
+    public static final long ONE_HOUR = 3600 * 1000;
+    public static final long TWELVE_HOURS = 12 * ONE_HOUR;
+
+    public static final long ONE_DAY = 24 * ONE_HOUR;
+    public static final long TWO_DAYS = 2 * ONE_DAY;
+}
